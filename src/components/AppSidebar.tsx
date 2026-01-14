0cf4ca505e0e68b3bd135980b53f0b53cbad5ec3
silentbuddha1 <ramchandrakarki231@gmail.com>
2026-01-14 19:57:59 +0545

    sidebar: mobile version

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -17,9 +17,14 @@ import { LogOutIcon } from 'lucide-react';
 import { Logo } from '../assets/Logo';
 
 import { APP_SIDEBAR } from '../constants';
+import { useEffect } from 'react';
 
 export const AppSidebar = () => {
-  const { isMobile, openMobile } = useSidebar();
+  const { isMobile, toggleSidebar } = useSidebar();
+
+  useEffect(() => {
+    toggleSidebar();
+  }, [toggleSidebar]);
   return (
     <Sidebar
       variant='floating'
@@ -32,6 +37,86 @@ export const AppSidebar = () => {
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {APP_SIDEBAR.primaryNav.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    asChild
+                  >
+                    <a href={item.url}>
+                      <item.Icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        {isMobile && (
+          <SidebarGroup className='mt-auto'>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {APP_SIDEBAR.secondaryNav.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      tooltip={item.title}
+                      asChild
+                    >
+                      <a href={item.url}>
+                        <item.Icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
+      </SidebarContent>
+
+      <SidebarFooter className={cn(isMobile && 'border-t')}>
+        <SidebarMenu>
+          <SidebarMenuItem className={cn(isMobile && 'p-2')}>
+            {isMobile ? (
+              <div className='flex justify-between items-start gap-2'>
+                <div className='grid grid-cols-[max-content_minmax(0,1fr)] items-center gap-2'>
+                  <div className='relative'>
+                    <Avatar
+                      src={APP_SIDEBAR.curProfile.src}
+                      size='36px'
+                      round='8px'
+                    />
+                    <div className='absolute bottom-0 right-0 size-2 rounded-full bg-emerald-500 dark:bg-emerald-400 ring-sidebar ring-1'></div>
+                  </div>
+                  <div>
+                    <h3 className='text-sm font-semibold'>
+                      {APP_SIDEBAR.curProfile.name}
+                    </h3>
+                    <p className='text-sm text-muted-foreground truncate'>
+                      {APP_SIDEBAR.curProfile.email}
+                    </p>
+                  </div>
+                </div>
+                <Button
+                  variant='ghost'
+                  size='icon-sm'
+                  aria-label='Log Out'
+                >
+                  <LogOutIcon />
+                </Button>
+              </div>
+            ) : (
+              <></>
+            )}
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
