810350c475bbb86bc77748c8af00e5cd303594b0
silentbuddha1 <ramchandrakarki231@gmail.com>
2026-01-15 01:40:23 +0545

    Page: PageHeader

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from './components/ThemeProvider';
 import { SidebarProvider, SidebarInset } from './components/ui/sidebar';
 import { AppSidebar } from './components/AppSidebar';
 import { Header } from '@/components/Header';
+import { Page, PageHeader } from '@/components/Page';
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
         <AppSidebar />
         <SidebarInset>
           <Header />
+          <main>
+            <Page>
+              <PageHeader />
+            </Page>
+          </main>
         </SidebarInset>
       </SidebarProvider>
     </ThemeProvider>
