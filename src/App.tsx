de6a523bc3b1fc7d5f0d246f1fe8b067a9d868df
silentbuddha1 <ramchandrakarki231@gmail.com>
2026-01-15 17:03:15 +0545

    Radial chart

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { AppSidebar } from './components/AppSidebar';
 import { Header } from '@/components/Header';
 import { Page, PageHeader } from '@/components/Page';
 import { DashboardCard } from '@/components/DashboardCard';
+import { AppBarChart } from '@/components/AppBarChart';
+import { AppRadialChart } from './components/AppRadialChart';
+import { TrendingUpIcon } from 'lucide-react';
 
 function App() {
   return (
@@ -15,13 +18,41 @@ function App() {
           <main>
             <Page>
               <PageHeader />
-              <div className=''>
+              <div className='grid gap-6 py-8 lg:grid-cols-[1fr_360px]'>
                 <DashboardCard
                   title='Dashboard'
                   description='Keep Track and Security ratings'
                   buttonText='View Full Report'
                 >
-                  {' '}
+                  <AppBarChart />
+                </DashboardCard>
+                <DashboardCard
+                  title='Monitor'
+                  description="You're using 80% of available spots."
+                  buttonText='Upgrade plan'
+                >
+                  <div className='flex justify-between items-start'>
+                    <AppRadialChart />
+                    <div className='flex items-center gap-2'>
+                      <TrendingUpIcon
+                        size={20}
+                        className='text-emerald-500 dark:text-emerald-400'
+                      />
+                      <span className='text-emerald-500 dark:text-emerald-400 font-medium'>
+                        {' '}
+                        10%
+                      </span>
+                    </div>
+                  </div>
+                  <div className='mt-6 lg:mt-8'>
+                    <p className='font-medium'>
+                      You're almost reached your limit
+                    </p>
+                    <p className='text-muted-foreground'>
+                      You have used 80% of your available spots. Upgrade plan to
+                      monitor more vendors
+                    </p>
+                  </div>
                 </DashboardCard>
               </div>
             </Page>
